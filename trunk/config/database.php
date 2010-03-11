2acf4bef486e14cb562fc51f79ab079f19aee70f
starlight36 <starlight36@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-03-11 05:51:53 +0000

    本地化database组件,使之适用于本系统

--- trunk/config/database.php
+++ trunk/config/database.php
@@ -1,51 +1,41 @@
 <?php if (!defined("IN_SYS")) die("Access Denied.");
 /*
 | -------------------------------------------------------------------
-| DATABASE CONNECTIVITY SETTINGS
+| 数据库连接设置文件
 | -------------------------------------------------------------------
-| This file will contain the settings needed to access your database.
-|
-| For complete instructions please consult the "Database Connection"
-| page of the User Guide.
+| 此文件用以包含连接数据库的必要设置.
 |
 | -------------------------------------------------------------------
-| EXPLANATION OF VARIABLES
+| 可用变量
 | -------------------------------------------------------------------
 |
-|	['hostname'] The hostname of your database server.
-|	['username'] The username used to connect to the database
-|	['password'] The password used to connect to the database
-|	['database'] The name of the database you want to connect to
-|	['dbdriver'] The database type. ie: mysql.  Currently supported:
+|	['hostname'] 数据库服务器主机地址.
+|	['username'] 数据库用户名
+|	['password'] 数据库密码
+|	['database'] 数据库名称
+|	['dbdriver'] 数据库驱动类型, 系统支持以下数据库:
 				 mysql, mysqli, postgre, odbc, mssql, sqlite, oci8
-|	['dbprefix'] You can add an optional prefix, which will be added
-|				 to the table name when using the  Active Record class
-|	['pconnect'] TRUE/FALSE - Whether to use a persistent connection
-|	['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
-|	['cache_on'] TRUE/FALSE - Enables/disables query caching
-|	['cachedir'] The path to the folder where cache files should be stored
-|	['char_set'] The character set used in communicating with the database
-|	['dbcollat'] The character collation used in communicating with the database
+|	['dbprefix'] 表名前缀
+|	['pconnect'] TRUE/FALSE - 是否启用持久连接
+|	['db_debug'] TRUE/FALSE - 是否为调试状态, 显示所有错误
+|	['cache_on'] TRUE/FALSE - 是否允许数据库缓存
+|	['char_set'] 内容字符集
+|	['dbcollat'] 数据库字符集
 |
-| The $active_group variable lets you choose which connection group to
-| make active.  By default there is only one group (the "default" group).
-|
-| The $active_record variables lets you determine whether or not to load
-| the active record class
+| 变量$active_group用以决定使用那组数据库连接设置
 */
 
 $active_group = "default";
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['username'] = "freewms";
+$db['default']['password'] = "freewms";
+$db['default']['database'] = "freewms";
 $db['default']['dbdriver'] = "mysql";
-$db['default']['dbprefix'] = "";
+$db['default']['dbprefix'] = "freewms_";
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = "";
 $db['default']['char_set'] = "utf8";
 $db['default']['dbcollat'] = "utf8_general_ci";
 
