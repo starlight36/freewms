7b7c2cb08c5dfd3e6175255988ef93f6fbd94269
sunboyqq16 <sunboyqq16@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-03-14 04:20:23 +0000

    增加memcache_cache模块，数据成员，以及补充了构造函数、析构函数

--- trunk/inc/library/cache/memcache_cache.php
+++ trunk/inc/library/cache/memcache_cache.php
@@ -3,14 +3,27 @@
  * memcache缓存类
  */
 class memcache_cache {
-	/**
+
+	private $mem = 0;
+        public $val = 0;
+
+
+        function  __construct() {
+                $mem = new Memcache;
+        }
+        function  __destruct() {
+                $mem->close();
+        }
+
+        /**
 	 * 设置缓存
 	 * @param string $key 缓存名
 	 * @param unknown $value 缓存内容
 	 * @return bool
 	 */
 	public function put($key, $value) {
-		
+                $mem->connect(MEMCACHE_HOST,MEMCACHE_PORT);
+                $mem->set($key,$value,0,CACHE_EXPIRE);
 		return TRUE;
 	}
 
@@ -20,6 +33,7 @@ class memcache_cache {
 	 * @return unknown
 	 */
 	public function get($key) {
+            $val = $mem->get($key);
 
 	}
 
@@ -29,6 +43,7 @@ class memcache_cache {
 	 * @return bool
 	 */
 	public function rm($key) {
+                $mem->delete($key);
 
 		return TRUE;
 	}
@@ -38,7 +53,7 @@ class memcache_cache {
 	 * @return bool
 	 */
 	public function rm_all() {
-
+                $mem->flush();
 		return TRUE;
 	}
 }
