de787061aa0c42976f2981c131b23a712ee3b008
aiyaren521@163.com <aiyaren521@163.com@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-05-05 09:08:41 +0000

    更新后台CSS样式

--- trunk/view/default/admin/module/modify.php
+++ trunk/view/default/admin/module/modify.php
@@ -1,9 +1,30 @@
 <form action="{#out:site_url('admin/module_modify/id/'.$mod_id)}" method="post">
-	<p>模块名称: <input type="text" name="name" id="name" value="{#out:set_value('name', $mod_name)}" /></p>
-	<p>模块简介: <input type="text" name="desc" id="desc" value="{#out:set_value('desc', $mod_desc)}" /></p>
-	<p>模块类库: <input type="text" name="class" id="class" value="{#out:set_value('class', $mod_class)}" /></p>
-	<p>模块插件: <input type="text" name="plugin" id="plugin" value="{#out:set_value('plugin', $mod_plugin)}" /></p>
-	<p>管理接口: <input type="text" name="manage" id="manage" value="{#out:set_value('manage', $mod_manage)}" /></p>
-	<input type="submit" value="保存" />
-	<input type="reset" value="重置" />
+	<table style="width: 100%" class="dialog_form">
+		<tr>
+			<td>模块在哪名称:</td>
+			<td><input type="text" name="name" id="name" value="{#out:set_value('name', $mod_name)}" /></td>
+		</tr>
+		<tr>
+			<td>模块简介:</td>
+			<td><input type="text" name="desc" id="desc" value="{#out:set_value('desc', $mod_desc)}" /></td>
+		</tr>
+		<tr>
+			<td>模块类库:</td>
+			<td><input type="text" name="class" id="class" value="{#out:set_value('class', $mod_class)}" /></td>
+		</tr>
+		<tr>
+			<td>模块插件:</td>
+			<td><input type="text" name="plugin" id="plugin" value="{#out:set_value('plugin', $mod_plugin)}" /></td>
+		</tr>
+		<tr>
+			<td>管理接口:</td>
+			<td><input type="text" name="manage" id="manage" value="{#out:set_value('manage', $mod_manage)}" /></td>
+		</tr>
+		<tr>
+			<td colspan="2" style="text-align: center;">
+				<input type="submit" value="保存" />
+				<input type="reset" value="重置" />
+			</td>
+		</tr>
+	</table>
 </form>
