fd577f1fa7d31beb7e3c30f6bec0d705274d98e7
wkl360 <wkl360@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-06-02 10:43:41 +0000

    为后台欢迎页菜单栏加入多语言支持

--- module/admin/templates/index.tpl.php
+++ module/admin/templates/index.tpl.php
@@ -27,16 +27,16 @@
 	<div id="topright">
 	  <div class="menubar">
 		<ul id="navlist">
-		  <li><a href="javascript:void(0)" title="关闭左边管理导航菜单" target="_self">关闭左栏</a></li>
-		  <li><a href="javascript:void(0)" title="对blog进行系统设置" target="_self">系统设置</a></li>
-		  <li><a href="javascript:void(0)" title="发表一篇新的日志" target="_self">发表日志</a></li>
-		  <li><a href="javascript:void(0)" title="设计制作主题风格" target="_self">风格设计</a></li>
-		  <li><a href="javascript:void(0)" title="生成首页静态文件" target="_self">生成首页</a></li>
-		  <li><a href="javascript:void(0)" title="打开前台首页" target="_self">前台首页</a></li>
-		  <li><a href="index.php?m=admin&amp;a=logout" title="退出后台管理" target="_self">退出后台</a></li>
+		  <li><a href="javascript:void(0)" title="<?php echo Lang::_('admin_close_left_menu_title');?>" target="_self"><?php echo Lang::_('admin_close_left_tip');?></a></li>
+		  <li><a href="javascript:void(0)" title="<?php echo Lang::_('admin_blog_set_title');?>" target="_self"><?php echo Lang::_('admin_sys_set_tip');?></a></li>
+		  <li><a href="javascript:void(0)" title="<?php echo Lang::_('admin_pub_new_log_title');?>" target="_self"><?php echo Lang::_('admin_pub_log_tip');?></a></li>
+		  <li><a href="javascript:void(0)" title="<?php echo Lang::_('admin_style_design_title');?>" target="_self"><?php echo Lang::_('admin_style_design_tip');?></a></li>
+		  <li><a href="javascript:void(0)" title="<?php echo Lang::_('admin_generated_page_title');?>" target="_self"><?php echo Lang::_('admin_generated_page_tip');?></a></li>
+		  <li><a href="javascript:void(0)" title="<?php echo Lang::_('admin_front_page_title');?>" target="_self"><?php echo Lang::_('admin_front_page_tip');?></a></li>
+		  <li><a href="index.php?m=admin&amp;a=logout" title="<?php echo Lang::_('admin_exit_index_title');?>" target="_self"><?php echo Lang::_('admin_exit_index_tip');?></a></li>
 		</ul>
 	  </div>
-		<div id="topmsg" style="display: none;"><img src="<?php echo Url::base();?>module/admin/images/loading.gif" alt="Loading" align="absmiddle"/>数据加载中，请稍候.....</div>
+		<div id="topmsg" style="display: none;"><img src="<?php echo Url::base();?>module/admin/images/loading.gif" alt="Loading" align="absmiddle"/><?php echo Lang::_('admin_load_tip');?></div>
 	</div>
 </div>
 <div id="leftframe">
@@ -44,72 +44,72 @@
 		<div id="menuall">
 			<div id="leftmenu" class="sdmenu">
 				<div class="collapsed">
-					<span class="content">网站内容管理</span>
-					<a onclick="return false;" title="发布内容" style="padding-left: 10px;">
+					<span class="content"><?php echo Lang::_('admin_content_tip');?></span>
+					<a onclick="return false;" title="<?php echo Lang::_('admin_release_content_title');?>" style="padding-left: 10px;">
 						<select onchange="window.main.location.href='?m=admin&a='+this.value;" style="width:140px">
-							<option value="main" selected="selected" disabled="disabled">选择分类发布新内容</option>
+							<option value="main" selected="selected" disabled="disabled"><?php echo Lang::_('admin_select_content_tip');?></option>
 							<?php echo $cate_select_tree; ?>
 						</select>
 					</a>
-					<a href="#" title="">全部内容管理</a>
-					<a href="#" title="">内容审核管理</a>
-					<a href="#" title="">内容分类管理</a>
-					<a href="#" title="">内容专题管理</a>
-					<a href="#" title="">内容推荐管理</a>
-					<a href="#" title="">草稿箱管理</a>
-					<a href="#" title="">回收站管理</a>
+					<a href="#" title=""><?php echo Lang::_('admin_all_content_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_review_content_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_classify_content_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_sub_content_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_recommend_content_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_draft_box_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_recycle_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="comment">留言评论管理</span>
-					<a href="#" title="">内容评论管理</a>
-					<a href="#" title="">评论审核管理</a>
-					<a href="#" title="">用户留言管理</a>
-					<a href="#" title="">留言审核管理</a>
+					<span class="comment"><?php echo Lang::_('admin_comment_tip');?></span>
+					<a href="#" title=""><?php echo Lang::_('admin_content_comment_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_comment_moderation_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_user_message_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_message_audit_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="create">静态生成管理</span>
-					<a href="#" title="">首页静态生成</a>
-					<a href="#" title="">分类页静态生成</a>
-					<a href="#" title="">专题页静态生成</a>
-					<a href="#" title="">内容页静态生成</a>
+					<span class="create"><?php echo Lang::_('admin_static_formation_tip');?></span>
+					<a href="#" title=""><?php echo Lang::_('admin_static_page_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_classify_page_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_sub_page_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_content_page_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="user">用户管理中心</span>
-					<a href="#" title="">全站用户管理</a>
-					<a href="#" title="">用户审核管理</a>
-					<a href="#" title="">用户组管理</a>
-					<a href="#" title="">管理员管理</a>
+					<span class="user"><?php echo Lang::_('admin_user_center_tip');?></span>
+					<a href="#" title=""><?php echo Lang::_('admin_all_user_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_user_audit_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_user_group_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_admin_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="theme">界面管理中心</span>
-					<a href="#" title="">主题模板管理</a>
-					<a href="#" title="">网站导航管理</a>
-					<a href="#" title="">自定义页管理</a>
-					<a href="#" title="">Widget管理</a>
+					<span class="theme"><?php echo Lang::_('admin_interface_tip');?></span>
+					<a href="#" title=""><?php echo Lang::_('admin_theme_templates_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_site_nav_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_costom_page_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_widget_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="sys">系统管理中心</span>
-					<a href="#" title="">全站设置管理</a>
-					<a href="#" title="">内容模型管理</a>
-					<a href="#" title="">上传文件管理</a>
-					<a href="#" title="">调查投票管理</a>
-					<a href="#" title="">友情链接管理</a>
-					<a href="#" title="">URL方案管理</a>
+					<span class="sys"><?php echo Lang::_('admin_sys_center_tip');?></span>
+					<a href="#" title=""><?php echo Lang::_('admin_survey_set_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_content_model_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_upload_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_vote_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_links_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_URL_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="data">数据管理中心</span>
-					<a href="#" title="">全站数据备份</a>
-					<a href="#" title="">数据备份恢复</a>
-					<a href="#" title="">数据库优化</a>
-					<a href="#" title="">执行SQL语句</a>
+					<span class="data"><?php echo Lang::_('admin_dm_tip');?></span>
+					<a href="#" title=""><?php echo Lang::_('admin_backup_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_resture_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_db_optimized_tip');?></a>
+					<a href="#" title=""><?php echo Lang::_('admin_SQL_tip');?></a>
 				</div>
 				<div class="collapsed">
-					<span class="support">支持与帮助</span>
-					<a href="#" title="取得官方参考手册">用户参考手册</a>
-					<a href="#" title="FreeWMS按照BSD协议开发,点此了解更多">授权使用协议</a>
-					<a href="#" title="取得专业的技术支持和二次开发">支持与额外服务</a>
-					<a href="http://code.google.com/p/freewms/" target="_blank" title="FreeWMS开源项目首页">FreeWMS项目组</a>
-					<a href="http://www.wedong.com" target="_blank" title="取得FreeWMS商业支持">维动网络</a>
+					<span class="support"><?php echo Lang::_('admin_support_help_tip');?></span>
+					<a href="#" title="<?php echo Lang::_('admin_user_reference_title');?>"><?php echo Lang::_('admin_user_reference_tip');?></a>
+					<a href="#" title="<?php echo Lang::_('admin_agreement_title');?>"><?php echo Lang::_('admin_agreement_tip');?></a>
+					<a href="#" title="<?php echo Lang::_('admin_service_title');?>"><?php echo Lang::_('admin_service_tip');?></a>
+					<a href="http://code.google.com/p/freewms/" target="_blank" title="<?php echo Lang::_('admin_pro_team_index_title');?>"><?php echo Lang::_('admin_pro_team_tip');?></a>
+					<a href="http://www.wedong.com" target="_blank" title="<?php echo Lang::_('admin_get_network_title');?>"><?php echo Lang::_('admin_network_tip');?></a>
 				</div>
 			</div>
 		</div>
