225889b0c6e991fa8843734cd30dd19831504c6e
starlight36 <starlight36@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-06-03 08:55:45 +0000

    添加内容列表模块

--- trunk/inc/content.class.php
+++ trunk/inc/content.class.php
@@ -120,16 +120,92 @@ class Content {
 	}
 
 	public function get_content_list($args, $pagesize = NULL, $pagenum = NULL) {
-		$list = Cache::get('content_list/'.md5($args));
+		//先检查是否在缓存中存在
+		$cache_name = 'content_list/'.md5($args);
+		$list = Cache::get($cache_name);
 		if($list != FALSE) {
 			return $list;
 		}
+		//解析参数标记
 		$args = path_array(Spyc::YAMLLoadString($args), 'args');
-		$sql = NULL;
-		if(!empty($args['where'])) {
-			$sql .= 'WHERE ';
 
+		$db = DB::get_instance();
+		$sql_where = array();
+		//指定专题
+		if(preg_match('/^[0-9]+$/', $args['subject'])) {
+			$sql_where[] = "`content_id` IN (SELECT `sc_contentid` FROM `".DB_PREFIX
+					."subject_content` WHERE `sc_subjectid` = '{$args['subject']}')";
 		}
+		//指定推荐位
+		if(preg_match('/^[0-9]+$/', $args['recommend'])) {
+			$sql_where[] = "`content_id` IN (SELECT `rc_contentid` FROM `".DB_PREFIX
+					."recommend_content` WHERE `rc_recid` = '{$args['recommend']}')";
+		}
+		//指定用户ID
+		if(preg_match('/^[0-9]+$/', $args['uid'])) {
+			$sql_where[] = "`content_userid` = '{$args['uid']}'";
+		}
+		//指定分类
+		if($args['category']) {
+			$cateinfo = $this->get_category($args['category']);
+			if($cateinfo == FALSE) {
+				return FALSE;
+			}
+			$cate_sql = "`content_cateid` = '{$cateinfo['cate_id']}'";
+			//级联选择
+			if($args['cascade'] && !empty($cateinfo['cate_childid'])) {
+				$cate_sql .= " OR `content_cateid` IN('".implode("','", $cateinfo['cate_childid'])."')";
+			}
+			$sql_where[] = $cate_sql;
+		}
+		//指定状态
+		if(preg_match('/^[0-9]+$/', $args['state'])) {
+			$sql_where[] = "`content_state` = '{$args['state']}'";
+		}
+		if(!empty($sql_where)) {
+			$sql_where = ' WHERE '.implode(' AND ', $sql_where);
+		}
+		//结果返回数
+		if(preg_match('/^[0-9]+$/', $args['limit'])) {
+			$sql_limit = ' LIMIT '.$args['limit'];
+		}
+		//如果指定了分页, 那么覆盖返回结果数, 同时计算总记录数
+		if($pagesize != NULL) {
+			$db->select('count(*)')->from('content')->sql_add($sql_where);
+			$record_count = $db->result($db->query()); //总记录数
+			$pagecount = ceil($record_count); //总分页数
+			$pagenum = is_null($pagenum) ? 1 : $pagenum;
+			$pagenum = $pagenum > $pagecount ? $pagenum : $pagecount;
+			$offset = ($pagenum - 1) * $pagesize;
+			$sql_limit = " LIMIT {$offset}, {$pagesize}";
+		}
+
+		//排序
+		if(!empty($args['order'])) {
+			$sql_order = ' ORDER BY ';
+			if(is_array($args['order'])) {
+				$sql_order .= implode(', ', $args['order']);
+			}else{
+				$sql_order .= $args['order'];
+			}
+		}
+
+		//查询数据库
+		$db->select('*')->from('content');
+		$db->sql_add($sql_where.$sql_order.$sql_limit);
+		$list = $db->get();
+		if($list == NULL) {
+			return FALSE;
+		}
+		foreach ($list as $row) {
+			$cate_info = $this->get_category($row['content_cateid']);
+			//处理URL
+			//未完成
+			$new_list[] = array_merge($row, $cate_info);
+		}
+		$list = $new_list;
+		Cache::set($cache_name, $list);
+		return $list;
 	}
 }
 
