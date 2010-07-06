73a12344fbe6aa3a1d8d08235d9f36467c3cf223
jueseliang <jueseliang@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-07-06 03:41:41 +0000

    修改分页bag  和制作filebrowser.inc.php中的分页

--- trunk/module/admin/filebrowser.inc.php
+++ trunk/module/admin/filebrowser.inc.php
@@ -39,6 +39,36 @@ if($_GET['do'] == 'savefile') {
 //--------------------------------------------
 //	文件列表
 //--------------------------------------------
+//  每页条数
+$pagesize = 2;
+//  分页总数
+$db->select('count( * )')->from('upload');
+$pagecount = $db->result($db->query());
+//  URl处理
+$url = 'index.php?'.$_SERVER["QUERY_STRING"];
+if(strpos('page=', $url) === FALSE) {
+	$url .= empty($url) ? 'page={page}':'&page={page}';
+}else{
+	$url = preg_replace('/page=(\d+)/i', 'page={page}', $url);
+}
+//  当前所在页
+if(!is_numeric($_GET['page'])) {
+	$pagenum = 1;
+}else{
+ $pagenum = $_GET['page'];
+}
+$offset = ($pagenum - 1)*$pagesize;
 
+//生成一个翻页导航条
+Paginate::set_paginate($url, $pagenum, $pagecount, $pagesize, 2);
+
+//分类选择树生成
+$db->select('*')->from('upload')->sql_add('ORDER BY `upload_time` DESC LIMIT '.$offset.", ".$pagesize);
+$temp=$db->get();
+foreach($temp as $row) {
+	echo "<P>".$row['upload_name'];
+	echo $row['upload_time']."</p>";
+}
+echo Paginate::get_paginate();
 //--------------------------------------------
 /* End of this file */
