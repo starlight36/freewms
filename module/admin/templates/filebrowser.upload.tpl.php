07648ca318a7c296bf3fc11c54cdbf003273e627
aiyaren521@163.com <aiyaren521@163.com@d78deca4-2cb5-11df-9578-218b88a8c395>
2010-07-10 09:55:36 +0000

    上传文件页更新

--- module/admin/templates/filebrowser.upload.tpl.php
+++ module/admin/templates/filebrowser.upload.tpl.php
@@ -11,3 +11,85 @@
  * 文件浏览器上传文件模版
  */
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>上传列表</title>
+<style type="text/css">
+<!--
+body {
+	padding: 0;
+	margin: 0;
+	width: 499px;
+	height: 268px;
+	overflow: hidden;
+}
+/*---------------main-style----------*/
+#main {
+	width: 100%;
+	height: 100%;
+	background-color: #f0f0ee;
+}
+.file {
+	border-style: solid;
+	border-color: #bbb;
+	border-width: 1px;
+	height: 256px;
+	float: left;
+	margin: 5px 0 5px 5px;
+	background-color: #fff;
+}
+.file_list {
+	width: 300px;
+}
+.file_upload {
+	width: 180px;
+	text-align: center;
+}
+
+/*--------------file-list---------------------*/
+.file_list input {
+	margin: 20px auto 0 20px;
+}
+.file_list p {
+	color: #333;
+	font-size: 12px;
+	margin: 20px auto auto 20px;
+}
+.file_upload input {
+	width: 100px;
+	height: 30px;
+	background-color: #89d325;
+	color:#FFF;
+	border: 0px;
+	margin-top: 80px;
+	font-size: 14px;
+	font-weight: bold;
+}
+-->
+</style>
+</head>
+
+<body>
+
+<div id="main">
+	<div class="file">
+    	<div class="file_list">
+        	<input type="file" />
+            <input type="file" />
+            <input type="file" />
+            <input type="file" />
+            <input type="file" />
+            <p>提示：您一次可以选择1-5个文件进行上传</p>
+    	</div>
+	</div>
+	<div class="file">
+    	<div class="file_upload">
+        	<input type="button" value="开始上传" />
+        </div>
+	</div>
+</div>
+
+</body>
+</html>
